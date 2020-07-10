8b12adda3fd9038a501255892c65e4f4d0d12fc0
Paul Franco <mail@paulfran.co>
2020-07-10 02:10:28 -0700

    Initial Commit

---
+++ Covid19/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Covid19
+//
+//  Created by Paul Franco on 7/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
